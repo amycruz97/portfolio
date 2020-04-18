2dc231b6cf0e07f7cdd50831e631ff48f79db5eb
amycruz97 <amarachiamaechi4@gmail.com>
2020-04-18 05:03:24 +0100

    progress-bar added

---
+++ index.js
@@ -0,0 +1,29 @@
+var html = function() {
+    document.querySelector('.html').className = "html1"
+}
+var css = function() {
+    document.querySelector('.css').className = "css1"
+}
+var javascript = function() {
+    document.querySelector('.javascript').className = "jav1"
+}
+var react = function() {
+    document.querySelector('.react').className = "react1"
+}
+var vue = function() {
+    document.querySelector('.vue').className = "vue1"
+}
+var sass = function() {
+    document.querySelector('.sass').className = "sass1"
+}
+var git = function() {
+    document.querySelector('.git').className = "git1"
+}
+
+setTimeout(html, 1000);
+setTimeout(css, 3000);
+setTimeout(javascript, 5000)
+setTimeout(react, 7000);
+setTimeout(vue, 9000);
+setTimeout(sass, 11000);
+setTimeout(git, 13000);
